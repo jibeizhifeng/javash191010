14f149d9d02d7acefdfaf8afae8f2d64ce9b488c
周泽进 <2211541668@qq.com>
2020-02-22 10:42:24 +0800

    初始化提交

---
+++ src/main/java/com/atguigu/common/consts/ScwAppConsts.java
@@ -0,0 +1,8 @@
+package com.atguigu.common.consts;
+
+//	常量接口，所有属性都是public static final
+public interface ScwAppConsts {
+	String PROJECT_CREATE_PREFIX = "project:create:temp";
+	
+	String PROJECT_DETAILS_KEY = "projectDetailsVo";
+}
---
+++ src/main/java/com/atguigu/common/vo/BaseVo.java
@@ -0,0 +1,9 @@
+package com.atguigu.common.vo;
+
+import lombok.Data;
+
+@Data
+public class BaseVo {
+	private String accessToken;
+}
+
